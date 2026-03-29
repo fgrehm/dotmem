8bce9ffc09f0b272502c2b027fb8250f30188f87
Fabio Rehm <fgrehm@gmail.com>
2026-03-29 18:21:11 -0300

    fix(version): follow cmdX pattern, add test

--- version.go
+++ version.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"runtime"
 
 	"github.com/spf13/cobra"
@@ -11,11 +12,16 @@ func newVersionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Show version and build information",
-		Run: func(cmd *cobra.Command, args []string) {
-			_, _ = fmt.Fprintf(cmd.OutOrStdout(), "dotmem version %s\n", version)
-			_, _ = fmt.Fprintf(cmd.OutOrStdout(), "  commit: %s\n", commit)
-			_, _ = fmt.Fprintf(cmd.OutOrStdout(), "  built:  %s\n", date)
-			_, _ = fmt.Fprintf(cmd.OutOrStdout(), "  go:     %s\n", runtime.Version())
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return cmdVersion(cmd.OutOrStdout())
 		},
 	}
+}
+
+func cmdVersion(w io.Writer) error {
+	_, err := fmt.Fprintf(w,
+		"dotmem version %s\n  commit: %s\n  built:  %s\n  go:     %s\n",
+		version, commit, date, runtime.Version(),
+	)
+	return err
 }
